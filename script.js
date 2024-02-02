bd1b7fe8fed40d5ec0f6c5edc55cfb7423c7f7a7
Annaliza_C <aldcadiente@gmail.com>
2024-02-01 16:38:20 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var tempCelsius = 10;
+var tempFahrenheit = tempCelsius * 1.8 + 32;
+
+console.log(
+  `Outside my friend's house it's ${tempCelsius} degrees celsius , which is ${tempFahrenheit} degrees fahrenheit.`
+);
